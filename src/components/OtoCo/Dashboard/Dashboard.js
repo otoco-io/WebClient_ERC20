ba98db2da40641ae390a7980a78f76577ec42592
Filipe Soccol <filipe.soccol@gmail.com>
2020-07-13 19:48:10 -0300

    Adjusting Series Agreement contract, supressing Wyoming from options.

--- src/components/OtoCo/Dashboard/Dashboard.js
+++ src/components/OtoCo/Dashboard/Dashboard.js
@@ -15,7 +15,7 @@ import Web3Integrate from './../../../web3-integrate';
 
 import {PDFAssembler} from 'pdfassembler';
 import fileSaver from 'file-saver';
-import pdfFile from '../../../images/DOA_de.pdf'
+import pdfFile from '../../../images/DOA_de2.pdf'
 import page1DE from '../../../images/page1_de.pdf'
 import page21DE from '../../../images/page21_de.pdf'
 
@@ -39,19 +39,22 @@ export default () => {
         let page1 = await fetch(page1DE).then(r => r.text());
         let page21 = await fetch(page21DE).then(r => r.text());
         // Replace texts on placeholders
-        page1 = page1.replace('{SERIES}', '+'+(info.name.length*300-3000)+' ('+info.name);
+        console.log(info.name, info.contract)
+        page1 = page1.replace('{SERIES}', (info.name.length*300-3000)+' ('+info.name);
+        page1 = page1.replace('0xXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', info.contract);
         page1 = page1.replace('DD/MM/YYYY', info.created.getUTCDate()+'/'+(info.created.getUTCMonth()+1)+'/'+info.created.getUTCFullYear());
         page1 = page1.replace('HH:MM',info.created.getUTCHours()+':'+(info.created.getUTCMinutes() < 10 ? '0'+info.created.getUTCMinutes() : info.created.getUTCMinutes()));
         page21 = page21.replace('0xXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', info.owner);
         // Create a new pdf based on Agreeement file
         const newPdf = new PDFAssembler(blob);
         newPdf.getPDFStructure().then(function(pdf) {
-            //console.log(pdf['/Root']['/Pages']['/Kids'][3]['/Contents']['stream']);
+            //console.log(pdf['/Root']['/Pages']['/Kids'][0]['/Contents']['stream']);
             //console.log(pdf['/Root']['/Pages']['/Kids'][20]['/Contents']['stream']);
             // Replace agreement pages for new ones
+            console.log(page1);
             pdf['/Root']['/Pages']['/Kids'][0]['/Contents']['stream'] = page1;
             pdf['/Root']['/Pages']['/Kids'][20]['/Contents']['stream'] = page21;
-            // Remove last page from Source file
+            //Remove last page from Source file
             pdf['/Root']['/Pages']['/Kids'].splice(-1);
             newPdf.assemblePdf('Series_Operation_Agreement.pdf')
             .then(function(pdfFile) {
